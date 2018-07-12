4ade3e1114205858e56114c8bbd86863ea66d099
Diego Munhoz <diego.fatec@hotmail.com>
2018-07-12 16:23:07 -0300

    Projeto GeraIFRelatorio

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import model.GeraIFRelatorio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GeraIFRelatorio quebraLote = new GeraIFRelatorio();
+		quebraLote.processar();
+
+		
+	}
+
+}
